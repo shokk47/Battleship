f9d9a6b68bc44711cb9a67b0f3e4881bf9f52426
shokk47 <gamerk789@gmail.com>
2023-05-21 13:00:54 +0200

    Aggiornate directory file

--- src/main/stampe.h
+++ src/main/stampe.h
@@ -1,14 +1,14 @@
 #ifndef BATTAGLIA_NAVALE_STAMPE_H
 #define BATTAGLIA_NAVALE_STAMPE_H
 
-#define SRC_REGOLAMENTO        "fileStampe/regolamento.txt"
-#define SRC_SCHERMATA_INIZIALE "fileStampe/schermataIniziale.txt"
-#define SRC_DIREZIONE          "fileStampe/direzione.txt"
-#define SRC_POS_AUTO_NAVI      "fileStampe/posizionareNaviAutomatico.txt"
-#define SRC_MOD_POS_NAVI       "fileStampe/modalitaPosizionamentoNavi.txt"
-#define SRC_FINE_TURNO         "fileStampe/fineTurno.txt"
-#define SRC_INIZIO_TURNO       "fileStampe/inizioTurno.txt"
-#define SRC_SALVATAGGIO        "salvataggio/dati.dat"
+#define SRC_REGOLAMENTO        "../files/regolamento.txt"
+#define SRC_SCHERMATA_INIZIALE "../files/schermataIniziale.txt"
+#define SRC_DIREZIONE          "../files/direzione.txt"
+#define SRC_POS_AUTO_NAVI      "../files/posizionareNaviAutomatico.txt"
+#define SRC_MOD_POS_NAVI       "../files/modalitaPosizionamentoNavi.txt"
+#define SRC_FINE_TURNO         "../files/fineTurno.txt"
+#define SRC_INIZIO_TURNO       "../files/inizioTurno.txt"
+#define SRC_SALVATAGGIO        "../backup/salvataggio/dati.dat"
 
 #define OFF                     0       // Open file failed
 #define WFF                     1       // Write file failed
